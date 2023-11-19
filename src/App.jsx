37e990fe1f7a0d1019428ea4ad1e1644e8ba3bb8
govind-maddi <maddigovindraj3@gmail.com>
2023-11-19 16:03:04 +0530

    updated remaining json.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./components/Layout";
 import './App.css'
 
 function App() {
+
+  const [ darkMode,setDarkMode ] = useState(false);
+
+  useEffect(() => {
+   if(darkMode)
+   {
+    const r = document.querySelector(':root');
+    let rs = getComputedStyle(r);
+    
+    r.style.setProperty('--contentsecondary', '#c9c1c1');
+    r.style.setProperty('--contentprimary', '#f5f5f5');
+    r.style.setProperty('--bgcolor', '#1e1b1b');
+   }
+   else
+   {
+    const r = document.querySelector(':root');
+    let rs = getComputedStyle(r);
+    
+    r.style.setProperty('--contentsecondary', '');
+    r.style.setProperty('--contentprimary', '');
+    r.style.setProperty('--bgcolor', '');
+   }
+
+  },[darkMode])
+
   return (
     <>
+      <span id="mode_cont" onClick={() => setDarkMode(!darkMode)}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.3807 2.01904C9.91573 3.38786 9 5.33708 9 7.50018C9 11.6423 12.3579 15.0002 16.5 15.0002C18.6631 15.0002 20.6123 14.0844 21.9811 12.6195C21.6613 17.8539 17.3149 22.0002 12 22.0002C6.47715 22.0002 2 17.523 2 12.0002C2 6.68532 6.14629 2.33888 11.3807 2.01904Z"></path></svg>
+      </span>
       <Layout/>
     </>
   )
--- src/components/Userdata/UserFilter.jsx
+++ src/components/Userdata/UserFilter.jsx
@@ -45,6 +45,12 @@ function UserFilter() {
   return (
     <div id='filter_cont'>
       <section id='search_cont'>
+        <span id='input_search_icon'>
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.25 3C5.3505 3 3 5.3505 3 8.25C3 11.1495 5.3505 13.5 8.25 13.5C11.1495 13.5 13.5 11.1495 13.5 8.25C13.5 5.3505 11.1495 3 8.25 3ZM1.5 8.25C1.5 4.52208 4.52208 1.5 8.25 1.5C11.9779 1.5 15 4.52208 15 8.25C15 11.9779 11.9779 15 8.25 15C4.52208 15 1.5 11.9779 1.5 8.25Z" fill="#050505"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9571 11.9572C12.25 11.6643 12.7249 11.6643 13.0178 11.9572L16.2803 15.2197C16.5732 15.5126 16.5732 15.9874 16.2803 16.2803C15.9874 16.5732 15.5125 16.5732 15.2196 16.2803L11.9571 13.0178C11.6642 12.7249 11.6642 12.2501 11.9571 11.9572Z" fill="#050505"/>
+        </svg>
+        </span>
         <input type="text" placeholder='Search' onChange={(e) => handleChange(e.target.value)}/>
       </section>
       <section className='filter_action_cont' id='filterbtn1'>
@@ -52,7 +58,7 @@ function UserFilter() {
           Trigger Reason
           <span className='sort_select_icon' style={{ transform : triggerSelect && 'rotate(0deg)' }}>
                 <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" clipRule="evenodd" d="M4.46967 0.46967C4.76256 0.176777 5.23744 0.176777 5.53033 0.46967L9.28033 4.21967C9.57322 4.51256 9.57322 4.98744 9.28033 5.28033C8.98744 5.57322 8.51256 5.57322 8.21967 5.28033L5 2.06066L1.78033 5.28033C1.48744 5.57322 1.01256 5.57322 0.71967 5.28033C0.426777 4.98744 0.426777 4.51256 0.71967 4.21967L4.46967 0.46967Z" fill="#1A1A1A"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M4.46967 0.46967C4.76256 0.176777 5.23744 0.176777 5.53033 0.46967L9.28033 4.21967C9.57322 4.51256 9.57322 4.98744 9.28033 5.28033C8.98744 5.57322 8.51256 5.57322 8.21967 5.28033L5 2.06066L1.78033 5.28033C1.48744 5.57322 1.01256 5.57322 0.71967 5.28033C0.426777 4.98744 0.426777 4.51256 0.71967 4.21967L4.46967 0.46967Z" fill="var(--contentprimary)"/>
                 </svg>
           </span>
         </button>
@@ -72,7 +78,7 @@ function UserFilter() {
           Risk Level
           <span className='sort_select_icon' style={{ transform : riskSelect && 'rotate(0deg)' }}>
                 <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" clipRule="evenodd" d="M4.46967 0.46967C4.76256 0.176777 5.23744 0.176777 5.53033 0.46967L9.28033 4.21967C9.57322 4.51256 9.57322 4.98744 9.28033 5.28033C8.98744 5.57322 8.51256 5.57322 8.21967 5.28033L5 2.06066L1.78033 5.28033C1.48744 5.57322 1.01256 5.57322 0.71967 5.28033C0.426777 4.98744 0.426777 4.51256 0.71967 4.21967L4.46967 0.46967Z" fill="#1A1A1A"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M4.46967 0.46967C4.76256 0.176777 5.23744 0.176777 5.53033 0.46967L9.28033 4.21967C9.57322 4.51256 9.57322 4.98744 9.28033 5.28033C8.98744 5.57322 8.51256 5.57322 8.21967 5.28033L5 2.06066L1.78033 5.28033C1.48744 5.57322 1.01256 5.57322 0.71967 5.28033C0.426777 4.98744 0.426777 4.51256 0.71967 4.21967L4.46967 0.46967Z" fill="var(--contentprimary)"/>
                 </svg>
           </span>
         </button>
